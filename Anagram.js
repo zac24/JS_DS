63be09033454c242ed1876809f725c9823583cb8
Prashant Dwivedi <prashant1.dwivedi@ril.com>
2020-09-19 00:58:17 +0530

    anagram changes

--- Anagram.js
+++ Anagram.js
@@ -34,46 +34,65 @@ console.log(
 
 // ================================================ Second Solution ===================================================
 
-function anagrams(firstString, secondString) {
-  const firstCharMap = buildCharMap(firstString);
-  const secondCharMap = buildCharMap(secondString);
-  if (Object.keys(firstCharMap).length !== Object.keys(secondCharMap).length)
-    return false;
+// function anagrams(firstString, secondString) {
+//   const firstCharMap = buildCharMap(firstString);
+//   const secondCharMap = buildCharMap(secondString);
+//   if (Object.keys(firstCharMap).length !== Object.keys(secondCharMap).length)
+//     return false;
 
-  for (let char in firstCharMap) {
-    if (firstCharMap[char] !== secondCharMap[char]) {
+//   for (let char in firstCharMap) {
+//     if (firstCharMap[char] !== secondCharMap[char]) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
+
+// function buildCharMap(inputString) {
+//   var inputArr = cleanLowerCaseString(inputString);
+
+//   var resultObj = {};
+//   for (var character of inputArr) {
+//     if (resultObj[character]) {
+//       resultObj[character] += 1;
+//     } else {
+//       resultObj[character] = 1;
+//     }
+//   }
+//   return resultObj;
+// }
+
+// function cleanLowerCaseString(inputString) {
+//   return inputString.replace(/[^\w]/g, "").toLowerCase().split("");
+// }
+
+
+
+function Anagram(str1, str2) {
+  if (str1 != undefined && str2 != undefined && str1.length == str2.length) {
+
+    var sortStr1 = Array.sort(str1.toLowerCase().replace(/[^\w]/g, '').toCharArray());
+    var sortStr2 = Array.sort(str2.toLowerCase().replace(/[^\w]/g, '').toCharArray());
+    if (sortStr1 == sortStr2) {
+      return true;
+    } else {
+      
       return false;
     }
+  } else {
+    return false;
   }
 
-  return true;
-}
 
-function buildCharMap(inputString) {
-  var inputArr = cleanLowerCaseString(inputString);
-
-  var resultObj = {};
-  for (var character of inputArr) {
-    if (resultObj[character]) {
-      resultObj[character] += 1;
-    } else {
-      resultObj[character] = 1;
-    }
-  }
-  return resultObj;
-}
-
-function cleanLowerCaseString(inputString) {
-  return inputString.replace(/[^\w]/g, "").toLowerCase().split("");
 }
 
 console.log(
   "anagrams('rail safety', 'fairy tales')",
-  anagrams("rail safety", "fairy tales")
+  Anagram("rail safety", "fairy tales")
 );
 
 console.log(
   "anagrams('Hi there', 'Bye there')",
-  anagrams("Hi there", "Bye there")
+  Anagram("Hi there", "Bye there")
 );
-
