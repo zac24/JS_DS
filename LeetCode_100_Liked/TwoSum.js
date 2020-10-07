43b35b900c3a8277c8d03a338ccdce9a4da97296
Prashant Dwivedi <dwi.pra24@gmail.com>
2020-10-08 01:51:44 +0530

    changes

--- LeetCode_100_Liked/TwoSum.js
+++ LeetCode_100_Liked/TwoSum.js
@@ -48,3 +48,7 @@ const twoSum = (nums, target) => {
 const nums = [2,7,11,15];
 const target = 9 ;
 console.log(twoSum(nums, target));
+
+
+
+
---
+++ Random/primeNumbers.js
@@ -0,0 +1,11 @@
+// - [ ] Print all prime numbers less than or equal to n.
+
+
+const primeNumbers = (n) => {
+    for (var i = 1; i < n; i++) {
+       
+    }
+}
+
+const input = 25;
+primeNumbers(input);
