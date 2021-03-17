a8a165928132e1a4e222172d749a82f4833d7e30
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-17 09:32:02 +0530

    added sliding window approach for average sum of subArrays

--- avgSumOfSubArrays.js
+++ avgSumOfSubArrays.js
@@ -4,22 +4,43 @@
 
 // ======= Brute force algorithm ======================
 
-function averageOfSubarrays(arr, K) {
-    var result = [];
-    for(var i = 0; i < arr.length - K+1; i++){
-        var sum = 0;
-        for(j=i; j<i+K; j++){
-            sum += arr[j];
+// function averageOfSubarrays(arr, K) {
+//     var result = [];
+//     for(var i = 0; i < arr.length - K+1; i++){
+//         var sum = 0;
+//         for(j=i; j<i+K; j++){
+//             sum += arr[j];
+//         }
+//         result.push(sum/K);
+//     }
+//     return result;
+// }
+
+// let inputArray = [1, 3, 2, 6, -1, 4, 1, 8, 2]
+// let K=5
+
+// console.log("average of subArrays ===>", averageOfSubarrays(inputArray, K))
+
+
+// ================= Sliding Window approach ==========================
+
+function avgSumOfSubarrays(arr, k) {
+    var result = []
+    var start = 0 
+    var sum = 0 
+    for(let end = 0; end < arr.length; end++){
+        sum += arr[end]
+        if(end >= k -1){
+            result.push(sum/k)
+            sum -= arr[start]
+            start += 1
+
         }
-        result.push(sum/K);
     }
-    return result;
+    return result
 }
 
 let inputArray = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 let K=5
 
-console.log("average of subArrays ===>", averageOfSubarrays(inputArray, K))
-
-
-// ================= Sliding Window approach ==========================
+console.log("average of subArrays ===>", avgSumOfSubarrays(inputArray, K))
