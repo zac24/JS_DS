7808b41452264dbec0cd260277c04cd5737b5da1
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-17 09:13:34 +0530

    added some change

--- avgSumOfSubArrays.js
+++ avgSumOfSubArrays.js
@@ -1,6 +1,9 @@
 // Given an array, find the average of all contiguous subarrays of size ‘K’ in it.
 // Array: [1, 3, 2, 6, -1, 4, 1, 8, 2], K=5
 
+
+// ======= Brute force algorithm ======================
+
 function averageOfSubarrays(arr, K) {
     var result = [];
     for(var i = 0; i < arr.length - K+1; i++){
@@ -17,3 +20,6 @@ let inputArray = [1, 3, 2, 6, -1, 4, 1, 8, 2]
 let K=5
 
 console.log("average of subArrays ===>", averageOfSubarrays(inputArray, K))
+
+
+// ================= Sliding Window approach ==========================
