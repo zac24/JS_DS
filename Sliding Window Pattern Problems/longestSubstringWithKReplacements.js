b146e72598c2d6f0606217d247d88dd576ef0ebc
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-20 00:11:37 +0530

    Longest Substring after k replacement

--- Sliding Window Pattern Problems/longestSubstringWithKReplacements.js
+++ Sliding Window Pattern Problems/longestSubstringWithKReplacements.js
@@ -24,6 +24,30 @@ Output: 3
 Explanation: Replace the 'b' or 'd' with 'c' to have the longest repeating substring "ccc".
  */
 
- function longestSubstringAfterKReplacement(arr, k){
-     
+ function longestSubstringAfterKReplacement(str, k){
+     var maxLength = 0 
+     var start = 0 
+     var charMap = {}
+     var maxRepeatCount = 0 
+     for(end = 0; end < str.length; end++){
+       let rightChar = str[end]
+        if(!(rightChar in charMap)){
+          charMap[rightChar] = 0
+        }
+        charMap[rightChar] += 1
+        maxRepeatCount = Math.max(maxRepeatCount, charMap[rightChar])
+
+        if((end - start + 1) - maxRepeatCount > k){
+          let leftChar = str[start]
+          charMap[leftChar] -= 1
+          start += 1
+        }
+      maxLength = Math.max(maxLength, end - start + 1)
+     }
+     return maxLength
  }
+
+ let input = 'aabccbb'
+ let k = 2
+
+ console.log('Longest Substring With k replacement =====>', longestSubstringAfterKReplacement(input, k))
