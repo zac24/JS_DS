4cdf6fad4eeb61a256b1dc5a8cef330527ae5963
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-29 23:29:58 +0530

    Problem Statement for `squaring a sorted array`

---
+++ Two Pointers/squareASortedArray.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Title : Squaring a Sorted Array
+ * 
+ * 
+ * Problem Statement #
+Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
+
+Example 1:
+
+Input: [-2, -1, 0, 2, 3]
+Output: [0, 1, 4, 4, 9]
+Example 2:
+
+Input: [-3, -1, 0, 1, 2]
+Output: [0 1 1 4 9]
+ * 
+ */
