f03eba532c6b4268a3a2309acd65b6ac9150401c
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-29 22:49:28 +0530

    solution for removeGivenDuplicate

--- Two Pointers/removeGivenDuplicate.js
+++ Two Pointers/removeGivenDuplicate.js
@@ -17,3 +17,22 @@ Explanation: The first two elements after removing every 'Key' will be [11, 1].
 Solution: This problem is quite similar to our parent problem. We can follow a two-pointer approach and shift numbers left upon encountering the ‘key’. Here is what the code will look like:
  * 
  */
+
+function removeGivenDuplicates (input, target) {
+    var nextElement = 0
+
+    var i = 0
+
+    while (i < input.length) {
+        if(input[i] !== target){
+            input[nextElement] = input[i]
+            nextElement +=1
+        }
+        i += 1
+    }
+
+    return nextElement
+}
+
+console.log ("The length of new Array after removing given Duplicates ====>", removeGivenDuplicates([3, 2, 3, 6, 3, 10, 9, 3], 3))
+console.log ("The length of new Array after removing given Duplicates ====>", removeGivenDuplicates([2, 11, 2, 2, 1], 2))
