c4dd7dbefab43603b0546ec538dff621716f973a
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-03-30 00:32:08 +0530

    solution for square of a Sorted Array

--- Two Pointers/squareASortedArray.js
+++ Two Pointers/squareASortedArray.js
@@ -16,3 +16,31 @@ Input: [-3, -1, 0, 1, 2]
 Output: [0 1 1 4 9]
  * 
  */
+
+
+function squareASortedArray(input) {
+    let n = input.length
+    var resulArray = Array(n).fill(0)
+    var highestSquareIndex = n - 1
+    var left = 0
+    var right = n-1
+
+    while (right > left) {
+        let leftSquare = input[left] * input[left]
+        let rightSquare = input[right] * input[right]
+
+        if(rightSquare >= leftSquare) {
+            resulArray[highestSquareIndex] = rightSquare
+            right -= 1
+        }else{
+            resulArray[highestSquareIndex] = leftSquare
+            left += 1
+        }
+        highestSquareIndex -= 1
+    }
+
+    return resulArray
+}
+
+console.log('The square of Sorted Array ====>', squareASortedArray([-2, -1, 0, 2, 3]))
+console.log('The square of Sorted Array ====>', squareASortedArray([-3, -1, 0, 1, 2]))
