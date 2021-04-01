a8cdb2ce8be96c637e465891676597fb2b8bf9ac
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-04-01 10:17:06 +0530

    revised tripet sum to zero

--- Two Pointers/tripletSumToZero.js
+++ Two Pointers/tripletSumToZero.js
@@ -20,7 +20,39 @@ Explanation: There are two unique triplets whose sum is equal to zero.
 
 function tripletSumToZero (input) {
     var resulArray = []
-    
+    let inputArray = input.sort()
+
+    for(let i = 0; i < inputArray.length; i++) {
+        if(i > 0 && inputArray[i] === inputArray[i-1]){
+            continue
+        }
+        search_pair(inputArray, -inputArray[i], i+1 , resulArray)
+    }
+    return resulArray
+}
+
+function search_pair(arr, target, left, resultArray) {
+    let right = arr.length - 1
+    while (left < right) {
+        let currentSum = arr[left] + arr[right]
+        if(currentSum === target){
+            resultArray.push([-currentSum, arr[left], arr[right]])
+            left += 1
+            right -= 1
+
+            while (left < right && arr[left] === arr[left - 1]){
+                left += 1
+            } 
+            while (left < right && arr[right] === arr[right - 1]){
+                right -= 1
+            }
+        }
+        else if(currentSum > target){
+            right -= 1
+        }else{
+            left += 1
+        }
+    }
 }
 
 
