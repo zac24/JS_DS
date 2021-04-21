cb1ceb5f9da292b951523552aa6132e996995d6e
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-04-21 11:28:24 +0530

    revision for removeDuplicates

--- Two Pointer Revsion1/RemoveDuplicates.js
+++ Two Pointer Revsion1/RemoveDuplicates.js
@@ -18,3 +18,22 @@ Explanation: The first two elements after removing the duplicates will be [2, 11
 
 // Solution: 
 
+function removeDuplicates(input) {
+    var nonDuplicate = 1
+    var i = 1
+    while (i < input.length) {
+        if(input[nonDuplicate - 1] !== input[i]){
+            input[nonDuplicate] = input[i]
+            nonDuplicate += 1
+        }
+        i += 1
+    }
+    return nonDuplicate
+}
+
+let input = [2, 3, 3, 3, 6, 9, 9]
+let input1 = [2, 2, 2, 11]
+
+
+console.log("Duplicate removed from Array ====>", removeDuplicates(input))
+console.log("Duplicate removed from Array ====>", removeDuplicates(input1))
