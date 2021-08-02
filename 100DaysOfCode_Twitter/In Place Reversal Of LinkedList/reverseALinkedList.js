12ebc6befce945849df437f818c280e6900ce47b
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-08-02 14:41:41 +0530

    Problem statement for revesing linkedList Inplace

---
+++ 100DaysOfCode_Twitter/In Place Reversal Of LinkedList/reverseALinkedList.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ * Reverse a LinkedList
+ * 
+ * Given the head of a Singly LinkedList, reverse the LinkedList. 
+ * Write a function to return the new head of the reversed LinkedList.
+ * 
+ *
+ */
+
