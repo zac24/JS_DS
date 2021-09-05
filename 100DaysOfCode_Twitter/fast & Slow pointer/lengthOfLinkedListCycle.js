00c0adb88c8c356cd5ded0d9ad3821b9bebe293d
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-09-05 11:40:39 +0530

    Day #11-Q2: find start of linkedList using modification

--- 100DaysOfCode_Twitter/fast & Slow pointer/lengthOfLinkedListCycle.js
+++ 100DaysOfCode_Twitter/fast & Slow pointer/lengthOfLinkedListCycle.js
@@ -46,7 +46,7 @@ head.next.next = new Node(3);
 head.next.next.next = new Node(4);
 head.next.next.next.next = new Node(5);
 head.next.next.next.next.next = new Node(6);
-// console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
+console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
 
 head.next.next.next.next.next.next = head.next.next;
 console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
---
+++ 100DaysOfCode_Twitter/fast & Slow pointer/startOfLinkedListCycle.js
@@ -0,0 +1,40 @@
+/**
+ * 
+ * Given the head of a Singly LinkedList that contains a cycle, 
+ * write a function to find the starting node of the cycle.
+ * 
+ */
+
+// with modifying LinkedList
+
+class Node {
+    constructor(value, next = null){
+        this.value = value
+        this.next = next
+    }
+}
+
+function startOfLinkedListCycle(head) {
+    while(head !== null){
+        if(head.visited){
+            return head
+        }
+        head.visited = true
+        head = head.next
+    }
+    return null
+}
+
+const head = new Node(1);
+head.next = new Node(2)
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+// console.log(`The start of the LinkedList Cycle`, startOfLinkedListCycle(head))
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`The start of the LinkedList Cycle`, startOfLinkedListCycle(head))
+
+// head.next.next.next.next.next.next = head.next.next.next;
+// console.log(`The start of the LinkedList Cycle`, startOfLinkedListCycle(head))
