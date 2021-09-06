5a7d0329c1e2b89bf0dd3efc658867376f2c9843
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-09-06 23:28:19 +0530

    Day #11-Q1: find the length of the cycle.

---
+++ 100DaysOfCode_Twitter/fast & Slow pointer/lengthOfLinkedListCycle.js
@@ -0,0 +1,55 @@
+/**
+ * 
+ * Given the head of a LinkedList with a cycle, find the length of the cycle.
+ * 
+ */
+
+
+class Node {
+    constructor(value, next=null){
+        this.value = value;
+        this.next = next
+    }
+}
+
+
+function lengthOfLinkedListCycle(head){
+    let slow = head
+    let fast = head
+    while(fast !== null && fast.next !== null){
+        slow = slow.next
+        fast = fast.next.next
+        if(slow === fast){
+            return lengthOfCycle(slow)
+        }
+    }
+    return 0
+}
+
+function lengthOfCycle(slow){
+    let current = slow
+    let count = 0
+    while(true){
+        current = current.next
+        count += 1
+        if(current === slow){
+            break;
+        }
+    }
+    return count
+}
+
+
+const head = new Node(1);
+head.next = new Node(2)
+head.next.next = new Node(3);
+head.next.next.next = new Node(4);
+head.next.next.next.next = new Node(5);
+head.next.next.next.next.next = new Node(6);
+// console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
+
+head.next.next.next.next.next.next = head.next.next;
+console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
+
+head.next.next.next.next.next.next = head.next.next.next;
+console.log(`The Linkedlist has cycle`, lengthOfLinkedListCycle(head))
--- 100DaysOfCode_Twitter/fast & Slow pointer/linkedListCycle.js
+++ 100DaysOfCode_Twitter/fast & Slow pointer/linkedListCycle.js
@@ -16,7 +16,7 @@ function hasCycle(head){
     let slow = head 
     let fast = head
 
-    while(fast !== null && fast !== null){
+    while(fast !== null && fast.next !== null){
         slow = slow.next
         fast = fast.next.next
         if(slow === fast){
