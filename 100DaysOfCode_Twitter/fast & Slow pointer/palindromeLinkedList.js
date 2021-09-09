2d7402870eb4e6973b674ededbc8e13219d8da37
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-09-09 22:39:47 +0530

    Day #13_Q2:- linkedlist palindrome

--- 100DaysOfCode_Twitter/fast & Slow pointer/palindromeLinkedList.js
+++ 100DaysOfCode_Twitter/fast & Slow pointer/palindromeLinkedList.js
@@ -15,3 +15,62 @@ Output: false
 
  * 
  */
+
+class Node{
+    constructor(value, next = null){
+        this.value = value
+        this.next = next
+    }
+}
+
+function palindromeLinkedList(head){
+    if(head === null || head.next === null){
+        return true
+    }
+    let slow = head
+    let fast = head   
+
+    while((fast !== null && fast.next !== null)){
+        slow = slow.next
+        fast = fast.next.next
+    }
+    headSecondHalf = reversedLinkedList(slow);
+    copyHeadSecondHalf = headSecondHalf;
+
+    while(head !== null && headSecondHalf !== null){
+        if(head.value !== headSecondHalf.value){
+            break;
+        }
+        head = head.next
+        headSecondHalf = headSecondHalf.next
+    }
+
+    reversedLinkedList(copyHeadSecondHalf)
+
+    if(head === null || headSecondHalf === null){
+        return true
+    }
+    return false
+}
+
+function reversedLinkedList(head){
+    let prev = null 
+    while(head !== null){
+        let next = head.next
+        head.next = prev
+        prev = head
+        head = next
+    }
+    return prev
+}
+
+const head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(1)
+// head.next.next.next = new Node(6)
+// head.next.next.next.next = new Node(4)
+// head.next.next.next.next.next = new Node(2)
+// head.next.next.next.next.next.next = new Node(1)
+
+console.log(`Is linkedList palindromic`, palindromeLinkedList(head))
+
