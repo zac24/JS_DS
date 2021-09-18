ebebb6d85c1a2fd8d333d2d3acce4f7cb5b85ea3
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-09-18 15:11:09 +0530

    Day #22_Q2: LinkedList Series_Q2: Third last Node of a Linked List

---
+++ 100DaysOfCode_Twitter/LinkedList/thirdNodeFromLastLinkedList.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * How do you find the third node from the end in a singly linked list?
+ * 
+ */
+
+class Node{
+    constructor(value, next = null){
+        this.value = value
+        this.next = next
+    }
+}
+
+function thirdLastNode(head) {
+    let slow = head
+    let fast = head
+    let length = 1
+
+    while(fast !== null && fast.next !== null){
+        fast = fast.next 
+        length += 1
+        
+        if(length > 3){
+            slow = slow.next
+        }
+    }
+
+    return slow.value
+}
+
+const head = new Node(1)
+head.next = new Node(2)
+head.next.next = new Node(3)
+head.next.next.next = new Node(4)
+head.next.next.next.next = new Node(5)
+
+console.log('1 -> 2 -> 3 -> 4 -> 5')
+console.log ('The third node from the end of linkedList ===>', thirdLastNode(head))
+
