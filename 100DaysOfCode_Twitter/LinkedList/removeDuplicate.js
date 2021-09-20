414ccbc31889aa2d4c7a56b862e19d468614c3dc
Prashant Dwivedi <dwi.pra24@gmail.com>
2021-09-21 01:46:05 +0530

    Day #29: LinkedList_Series_Q10: improved solution for remove Duplicate question

--- 100DaysOfCode_Twitter/LinkedList/removeDuplicate.js
+++ 100DaysOfCode_Twitter/LinkedList/removeDuplicate.js
@@ -4,44 +4,64 @@
  * 
  */
 
-class Node{
-    constructor(value, next = null){
+class Node {
+    constructor(value, next = null) {
         this.value = value
-        this.next = next 
+        this.next = next
     }
 }
 
-function removeDuplicates(head){
-    let prev = head
+// function removeDuplicates(head){
+//     let prev = head
+//     let current = head
+
+//     let result = new Node ()
+//     let resultHead = result
+
+//     while(head !== null){
+//         if(prev.value === current.value){
+//             current = current.next
+//         }else {
+//             result.next = new Node(prev.value)
+//             result = result.next
+//             prev = current
+//             current = current.next
+//         }
+//         head = head.next
+//     }
+
+//     if(head === null){
+//         result.next = new Node(prev.value)
+//         result = result.next
+//     }
+
+//     return resultHead.next
+// }
+
+function removeDuplicates(head) {
+    
+    if (!head) {
+        return head
+    }
+
     let current = head
-
-    let result = new Node ()
-    let resultHead = result
-
-    while(head !== null){
-        if(prev.value === current.value){
-            current = current.next
+    while(current != null && current.next != null){
+        if(current.next.value === current.value){
+            current.next = current.next.next
         }else {
-            result.next = new Node(prev.value)
-            result = result.next
-            prev = current
             current = current.next
         }
-        head = head.next
     }
-
-    if(head === null){
-        result.next = new Node(prev.value)
-        result = result.next
-    }
-
-    return resultHead.next
+    return head
 }
 
-const head = new Node(1)
-head.next = new Node(1)
-head.next.next = new Node(2)
-head.next.next.next = new Node(3)
-head.next.next.next.next = new Node(3)
+const head = new Node(7)
+head.next = new Node(7)
+head.next.next = new Node(7)
+head.next.next.next = new Node(2)
+head.next.next.next.next = new Node(4)
+head.next.next.next.next = new Node(7)
+head.next.next.next.next.next = new Node(7)
+head.next.next.next.next.next.next = new Node(7)
 
 console.log('The removed duplicate ===>', removeDuplicates(head))
